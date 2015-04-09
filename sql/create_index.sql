c8258a7d75e1064c3502a371feeb387376ca87b2
Scott Rosenbaum <scottr@innoventsolutions.com>
2015-04-09 16:15:49 -0500

    get rid of old files, rework flow of master

--- sql/create_index.sql
+++ sql/create_index.sql
@@ -1,5 +1,14 @@
 create index K_UPDATEDATE on ORDER(updatedate,updatetime);
-create index K_UPDATEDATE on ENCORDR(updatedate,updatetime);
-create index K_UPDATEDATE on ENCOUNTR(updatedate,updatetime);
-create index K_UPDATEDATE on INSURANC(updatedate,updatetime);
-create index K_UPDATEDATE on PATIENT(updatedate,updatetime);
+
+-- fairly quick, did not see it happen
+-- create index K_UPDATEDATE on ENCORDR(updatedate,updatetime);
+
+-- About 30 minutes
+-- create index K_UPDATEDATE on ENCOUNTR(updatedate,updatetime);
+
+-- INSTANT
+-- create index K_UPDATEDATE on INSURANC(updatedate,updatetime);
+
+-- ABOUT ONE HOUR
+-- create index K_UPDATEDATE on PATIENT(updatedate,updatetime);
+

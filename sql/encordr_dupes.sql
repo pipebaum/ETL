5c557534e4247c7bdff2ce8ecd757bbfeca5130d
Scott Rosenbaum <scottr@innoventsolutions.com>
2015-04-16 10:19:23 -0500

    use ${SMP_ETL_SCHEMA} in all

---
+++ sql/encordr_dupes.sql
@@ -0,0 +1,157 @@
+select * from encordr where encounter = 12027239 and procedr = 'VITADH'
+union
+select * from encordr where encounter = 12090766 and procedr = 'VITADH'
+union
+select * from encordr where encounter = 12097417 and procedr = 'VITADH'
+union
+select * from encordr where encounter = 13011633 and procedr = 'VITADH'
+union
+select * from encordr where encounter = 14023740 and procedr = 'VITADH'
+union
+select * from encordr where encounter = 14085576 and procedr = 'VITADH'
+union
+select * from encordr where encounter = 14098893 and procedr = 'VITADH'
+union
+select * from encordr where encounter = 10053752 and procedr = 'UA'
+union
+select * from encordr where encounter = 10093341 and procedr = 'UA'
+union
+select * from encordr where encounter = 11031035 and procedr = 'UA'
+union
+select * from encordr where encounter = 11126262 and procedr = 'UA'
+union
+select * from encordr where encounter = 13014781 and procedr = 'UA'
+union
+select * from encordr where encounter = 10042790 and procedr = 'U'
+union
+select * from encordr where encounter = 10031503 and procedr = 'TSH'
+union
+select * from encordr where encounter = 10077475 and procedr = 'TSH'
+union
+select * from encordr where encounter = 10115623 and procedr = 'TSH'
+union
+select * from encordr where encounter = 12011123 and procedr = 'TSH'
+union
+select * from encordr where encounter = 12019035 and procedr = 'TSH'
+union
+select * from encordr where encounter = 12021196 and procedr = 'TSH'
+union
+select * from encordr where encounter = 12085362 and procedr = 'TSH'
+union
+select * from encordr where encounter = 13046961 and procedr = 'TSH'
+union
+select * from encordr where encounter = 14011067 and procedr = 'TSH'
+union
+select * from encordr where encounter = 14026278 and procedr = 'TSH'
+union
+select * from encordr where encounter = 14037498 and procedr = 'TSH'
+union
+select * from encordr where encounter = 14098893 and procedr = 'TSH'
+union
+select * from encordr where encounter = 12012236 and procedr = 'RPR'
+union
+select * from encordr where encounter = 13065647 and procedr = 'RPR'
+union
+select * from encordr where encounter = 10090525 and procedr = 'RA'
+union
+select * from encordr where encounter = 10020334 and procedr = 'NB'
+union
+select * from encordr where encounter = 10061548 and procedr = 'NB'
+union
+select * from encordr where encounter = 10064695 and procedr = 'NB'
+union
+select * from encordr where encounter = 10112788 and procedr = 'NB'
+union
+select * from encordr where encounter = 11056908 and procedr = 'NB'
+union
+select * from encordr where encounter = 14024117 and procedr = 'MG'
+union
+select * from encordr where encounter = 12078884 and procedr = 'LIPA'
+union
+select * from encordr where encounter = 12079615 and procedr = 'LIPA'
+union
+select * from encordr where encounter = 11035362 and procedr = 'HIV'
+union
+select * from encordr where encounter = 11093960 and procedr = 'HIV'
+union
+select * from encordr where encounter = 12012236 and procedr = 'HIV'
+union
+select * from encordr where encounter = 12019994 and procedr = 'HIV'
+union
+select * from encordr where encounter = 14104323 and procedr = 'HIV'
+union
+select * from encordr where encounter = 14011067 and procedr = 'HCGQNT'
+union
+select * from encordr where encounter = 14052682 and procedr = 'G8703'
+union
+select * from encordr where encounter = 14090859 and procedr = 'G1201'
+union
+select * from encordr where encounter = 15012979 and procedr = 'FT4'
+union
+select * from encordr where encounter = 11003665 and procedr = 'ESR'
+union
+select * from encordr where encounter = 11010688 and procedr = 'ESR'
+union
+select * from encordr where encounter = 11025545 and procedr = 'ESR'
+union
+select * from encordr where encounter = 11052352 and procedr = 'ESR'
+union
+select * from encordr where encounter = 12103074 and procedr = 'CULUR'
+union
+select * from encordr where encounter = 13004025 and procedr = 'CULUR'
+union
+select * from encordr where encounter = 10116100 and procedr = 'CRP'
+union
+select * from encordr where encounter = 14098893 and procedr = 'CRP'
+union
+select * from encordr where encounter = 13039528 and procedr = 'CREAT'
+union
+select * from encordr where encounter = 10054134 and procedr = 'CMP'
+union
+select * from encordr where encounter = 10116341 and procedr = 'CMP'
+union
+select * from encordr where encounter = 11123603 and procedr = 'CMP'
+union
+select * from encordr where encounter = 12037919 and procedr = 'CMP'
+union
+select * from encordr where encounter = 14075015 and procedr = 'CMP'
+union
+select * from encordr where encounter = 11018914 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11019751 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11051507 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11059498 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11060209 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11060218 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11090489 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11117798 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 11130933 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 14096321 and procedr = 'CBCD'
+union
+select * from encordr where encounter = 13039528 and procedr = 'BUN'
+union
+select * from encordr where encounter = 14024117 and procedr = 'BUN'
+union
+select * from encordr where encounter = 10053752 and procedr = 'B12'
+union
+select * from encordr where encounter = 10108069 and procedr = 'B12'
+union
+select * from encordr where encounter = 10115065 and procedr = 'B12'
+union
+select * from encordr where encounter = 11002257 and procedr = 'B12'
+union
+select * from encordr where encounter = 11104766 and procedr = 'B12'
+union
+select * from encordr where encounter = 12011123 and procedr = 'B12'
+union
+select * from encordr where encounter = 14085576 and procedr = 'B12'
+union
+select * from encordr where encounter = 11012790 and procedr = 'AMYL'

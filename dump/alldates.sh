185289998b65b8914c92a2bc3d20989ef73b0395
scottr <scottr@innoventsolutions.com>
2015-04-16 16:38:58 -0500

    Add top level job to control flow beyond load

--- dump/alldates.sh
+++ dump/alldates.sh
@@ -1,1 +1,1 @@
-mysqldump -uroot -pH1ghcr0ft horizon_duplicate doctors profile procedur facility rescodes insuranc | gzip > alldates.dump.gz
+mysqldump -uroot -pH1ghcr0ft smpetl encountr encordr patient doctors profile procedur facility rescodes insuranc | gzip > alldates.dump.gz

86d289c91a65e080615528bd7db59f8200e1baf5
Scott Rosenbaum <scottr@innoventsolutions.com>
2015-04-29 08:42:37 -0500

    make dump path relative

--- scripts/sql_dump_all.sh
+++ scripts/sql_dump_all.sh
@@ -1,16 +1,16 @@
 # $1 = user
 # $2 = password
 # $3 = database
-mysqldump -u$1 -p$2 $3 encountr encordr patient doctors profile procedur facility rescodes insuranc | gzip > ~/pdi/ETL/dump/alldates.dump.gz
-if [ ! -f ~/pdi/ETL/dump/alldates.dump.gz ]; then
+mysqldump -u$1 -p$2 $3 encountr encordr patient doctors profile procedur facility rescodes insuranc | gzip > ../dump/alldates.dump.gz
+if [ ! -f ../dump/alldates.dump.gz ]; then
    echo "File alldates does not exist after dump"
    exit 1
 fi
 
-mysqldump -u$1 -p$2 $3 orders --where="updatedate>=DATE_SUB(NOW(),INTERVAL 1 YEAR)" | gzip > ~/pdi/ETL/dump/datelimit.dump.gz
-if [ ! -f ~/pdi/ETL/dump/alldates.dump.gz ]; then
+mysqldump -u$1 -p$2 $3 orders --where="updatedate>=DATE_SUB(NOW(),INTERVAL 1 YEAR)" | gzip > ../dump/datelimit.dump.gz
+if [ ! -f ../dump/alldates.dump.gz ]; then
    echo "File datelimits does not exist after dump"
    exit 1
 fi
 
-touch ~/pdi/ETL/dump/dump_complete_$(date +"%m_%d_%Y_%H_%M_%S").txt
+touch ../dump/dump_complete_$(date +"%m_%d_%Y_%H_%M_%S").txt
--- scripts/transfer_2_aws.sh
+++ scripts/transfer_2_aws.sh
@@ -1,9 +1,9 @@
 # move the dump file
-scp -i ~/pdi/SMPLABS_AWSKeyPair.pem ~/pdi/ETL/dump/*.gz 54.69.142.168:~
+scp -i ~/pdi/SMPLABS_AWSKeyPair.pem ../dump/*.gz 54.69.142.168:~
 echo "moved db files"
 
 # move the run script - use latest version
-scp -i ~/pdi/SMPLABS_AWSKeyPair.pem ~/pdi/ETL/aws/*.* 54.69.142.168:~
+scp -i ~/pdi/SMPLABS_AWSKeyPair.pem ../aws/*.* 54.69.142.168:~
 echo "moved script files"
 
-touch ~/pdi/ETL/dump/xfer_complete_$(date +"%m_%d_%Y_%H_%M_%S").txt
+touch ../dump/xfer_complete_$(date +"%m_%d_%Y_%H_%M_%S").txt

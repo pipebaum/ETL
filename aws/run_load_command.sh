515123a5b28c2e904f36589b052e42b5ca9f2545
Scott Rosenbaum <scottr@innoventsolutions.com>
2015-04-29 08:30:21 -0500

    more work on scripts

--- aws/run_load_command.sh
+++ aws/run_load_command.sh
@@ -1,3 +1,7 @@
+# $1 = user
+# $2 = password
+# $3 = database
+
 function getDate(){
    _now=$(date +"%m_%d_%Y_%H_%M_%S")
    echo "$_now"
@@ -18,15 +22,15 @@ else
    echo "$(getDate) File datelimit does not exist" 1>&2
    exit 1
 fi
-gunzip alldates.dump.gz datelimit.dump.gz
-echo "$(getDate) files unzipped"
-mysql -uscottr -p$1 smpetl < alldates.dump
+
+zcat alldates.dump.gz | mysql -u $1 -p$2 -D$3 -B
 echo "$(getDate) alldates loaded"
-mysql -uscottr -p$1 smpetl < datelimit.dump
+
+zcat datelimit.dump.gz | mysql -u $1 -p$2 -D$3 -B
 echo "$(getDate) datelimit loaded"
 
 _file_out="backup_$(getDate).gz"
 
-tar -zcvf "./olddump/$_file_out" *.dump
-rm *.dump
+tar -zcvf "./olddump/$_file_out" *.tgz
+rm *.gz
 echo "$(getDate) delete source and archive to $_file_out"

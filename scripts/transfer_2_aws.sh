5dd7f079d121b99fd2d19198fba10e86e85cbbc9
Scott Rosenbaum <scottr@innoventsolutions.com>
2015-05-04 18:21:09 -0500

    modify transfer script to accept pem file as parameter

--- scripts/transfer_2_aws.sh
+++ scripts/transfer_2_aws.sh
@@ -1,9 +1,11 @@
+#1 location of .pem file
+
 # move the dump file from local to the remote AWS server
-scp -i ~/pdi/SMPLABS_AWSKeyPair.pem ~/pdi/etl/ETL/dump/*.gz 54.69.142.168:~
+scp -i $1 ~/pdi/etl/ETL/dump/*.gz 54.69.142.168:~
 echo "moved db files"
 
 # move the run script - use latest version
-scp -i ~/pdi/SMPLABS_AWSKeyPair.pem ~/pdi/etl/ETL/aws/*.* 54.69.142.168:~
+scp -i $1 ~/pdi/etl/ETL/aws/*.* 54.69.142.168:~
 echo "moved script files"
 
 touch ~/pdi/etl/ETL/dump/xfer_complete_$(date +"%m_%d_%Y_%H_%M_%S").txt

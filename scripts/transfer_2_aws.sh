62687ae8939a95ee5a0648c85cb18a9620d64e7c
scottr <scottr@innoventsolutions.com>
2015-05-05 12:49:50 -0500

    more path adjustments

--- scripts/transfer_2_aws.sh
+++ scripts/transfer_2_aws.sh
@@ -1,11 +1,12 @@
 #1 location of .pem file
+#2 root directory for ETL
 
 # move the dump file from local to the remote AWS server
-scp -i $1 ~/pdi/etl/ETL/dump/*.gz 54.69.142.168:~
+scp -i $1 $2/dump/*.gz 54.69.142.168:~
 echo "moved db files"
 
 # move the run script - use latest version
-scp -i $1 ~/pdi/etl/ETL/aws/*.* 54.69.142.168:~
+scp -i $1 $2/aws/*.* 54.69.142.168:~
 echo "moved script files"
 
-touch ~/pdi/etl/ETL/dump/xfer_complete_$(date +"%m_%d_%Y_%H_%M_%S").txt
+touch $2/dump/xfer_complete_$(date +"%m_%d_%Y_%H_%M_%S").txt

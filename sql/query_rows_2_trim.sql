f98f2158667c86307e9096224e0d7d4f5876aadb
Scott Rosenbaum <scottr@innoventsolutions.com>
2015-05-22 17:12:28 -0500

    Add files to trim for delta loads

---
+++ sql/query_rows_2_trim.sql
@@ -0,0 +1,48 @@
+use smpetl;
+
+select 'doctor', 12, count(*)
+from doctors
+where updatedate > '2015-03-15'
+union all
+select 'encordr', -1 , count(*)
+from encordr
+where updatetime > '15:44:32'
+and updatedate >= '2015-04-08'
+union all
+select 'encountr', -1 , count(*)
+from encountr
+where updatetime > '15:44:32'
+and updatedate >= '2015-04-08'
+union all
+select 'facility', 17, count(*)
+from facility
+where updatedate > '2015-03-15'
+union all
+select 'insuranc', 12, count(*)
+from insuranc
+where updatetime > '14:45:00'
+and updatedate >= '2013-09-23'
+union all
+select 'orders', 103, count(*)
+from orders
+where updatetime > '19:40:00'
+and updatedate > '2015-04-07'
+union all
+select 'patient', 24, count(*)
+from patient
+where updatetime > '17:00:00'
+and updatedate > '2015-04-07'
+union all
+select 'procedur', 0, count(*)
+from procedur
+where updatedate > '2015-04-07'
+union all
+select 'profile', 4, count(*)
+from profile
+where updatedate > '2015-03-15'
+union all
+select 'rescodes', 69, count(*)
+from rescodes
+where updatedate > '2014-08-12';
+
+
